47b49ff2f9ec67439bcaaaf2c4df38ad39ac2d05
PRONOB BISWAS <biswaspronob@outlook.com>
2024-01-25 05:24:23 +0600

    duiti Operand k + operator dwara jog kora somvob holo

--- js/script.js
+++ js/script.js
@@ -2,22 +2,30 @@
 let heading = document.querySelector("#hello");
 let result = document.querySelector("#result");
 
-
-// ======change====
-
-result.onfullscreenchange = cgn;
- function cng(){
-    if (result.innerHTML.length > 4) {
-        result.innerHTML = result.innerHTML.slice(0,1);
-      }
-};
-
 // =======number btn==========
 let eqqale = document.querySelector("#eqqale");
 let clears = document.querySelector("#clears");
+// =======number btn==========
+
+
+// ======change====
+let logRessult = result.innerHTML;
+// ======change====
+
+
 
 // ========button inovoketion=========
 clears.addEventListener("click", function(){
     console.log(result.innerHTML);
-    
+    console.log(typeof result.innerHTML);
+
+    let myArray = result.innerHTML.split("+");
+    let firstOperand = parseInt(myArray[0]);
+    let secondOperand = parseInt(myArray[1]);
+
+    console.log(myArray);
+    console.log( firstOperand);
+    console.log( secondOperand);
+    console.log(firstOperand + secondOperand);
+
 })
