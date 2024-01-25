c238ece49089fb8479d63669c1084eb067232311
PRONOB BISWAS <biswaspronob@outlook.com>
2024-01-25 06:23:49 +0600

    trying to work with multipule operand bya an meathmetical operation like plus

--- js/script.js
+++ js/script.js
@@ -22,10 +22,29 @@ clears.addEventListener("click", function(){
     let myArray = result.innerHTML.split("+");
     let firstOperand = parseInt(myArray[0]);
     let secondOperand = parseInt(myArray[1]);
+    let thirdOperand = parseInt(myArray[2]);
+    let txt = "";
+
+    // myArray.forEach(myFunctioN);
+    // function myFunctioN(value, index, array){
+    //     txt += parseInt(value);
+    //     console.log(txt)
+
+    //     let i,len,text;
+    // for (i = 0, len = myArray.length, text = ""; i < len; i++){
+    //     text += parseInt(myArray[0]);
+    //     console.log(text)
+    // }
+    // }
 
     console.log(myArray);
     console.log( firstOperand);
     console.log( secondOperand);
     console.log(firstOperand + secondOperand);
+    // let i,len,text;
+    // for (i = 0, len = myArray.length, text = ""; i < len; i++){
+    //     text += myArray[0];
+    //     console.log(text)
+    // }
 
 })
